021c5f5d6d60a85718b487b88e3747c3af5a5440
Joseph Neil Gapuz <josephneil873@gmail.com>
2025-09-18 17:09:59 +0800

    Upgrade SDK 53 to 54

--- components/tabbar/TabBar.tsx
+++ components/tabbar/TabBar.tsx
@@ -62,14 +62,14 @@ function TabBar({ state, descriptors, navigation }: BottomTabBarProps) {
 					typeof options.tabBarLabel === "string"
 						? options.tabBarLabel
 						: options.title !== undefined
-							? options.title
-							: route.name;
+						? options.title
+						: route.name;
 
 				const is_focused = state.index === index;
 
 				const onPress = () => {
 					tab_position_x.value = withSpring(button_width * index, {
-						duration: 1500,
+						duration: 500,
 					});
 					const event = navigation.emit({
 						type: "tabPress",
